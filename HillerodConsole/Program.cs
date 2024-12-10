69a4284db98711d089d77150f3d7f1e9e4ec806c
Nicolei Sander Dalgaard <nid003@edu.zealand.dk>
2024-12-10 11:00:55 +0100

    new

--- HillerodConsole/Program.cs
+++ HillerodConsole/Program.cs
@@ -1,3 +1,10 @@
 ﻿using hillerodLib;
 
+
+
 Class1 class1 = new Class1();
+BoatRepo repo = new BoatRepo();
+
+repo.AddBoat(new(1, "Molly", "SailBoat", "Beneteau395", "395", "Yanmar 4JH4", "12", "2018"));
+repo.AddBoat(new(2, "Dori", "Sailboat", "Shantau245", "245", "Volvo 4kMA", "14", "2014"));
+repo.AddBoat(new(3, "Maren", "Sailboat", "Nimbus 405 Coupe", "N405", "2 x Volvo Penta 380HK", "16", "2020"));
---
+++ hillerodLib/BoatRepo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hillerodLib
+{
+    public class BoatRepo
+    {
+        private Dictionary<int, Boat> _boatList = new Dictionary<int, Boat>();
+
+        // Så man kan tilføje en båd
+        public void AddBoat(Boat boat)
+
+        {
+            _boatList.TryAdd(boat.Id, boat);
+        }
+
+        // Så man kan slette en båd
+        public bool DeleteBoat(int boatId, out Boat deleteBoat)
+        {
+            return _boatList.Remove(boatId, out deleteBoat);
+
+        }
+
+        // Så man kan opdatere en båd
+        public void UpdateBoat(int id, Boat boat)
+        {
+            if (_boatList.ContainsKey(id))
+            {
+                _boatList[id].Name = boat.Name;
+                _boatList[id].Engine = boat.Engine;
+                _boatList[id].SailNumber = boat.SailNumber;
+                _boatList[id].Type = boat.Type;
+                _boatList[id].Measures = boat.Measures;
+                _boatList[id].BuildingYear = boat.BuildingYear;
+                _boatList[id].Model = boat.Model;
+            }
+
+        }
+
+        // Så man kan finde en båd på Id nummer
+
+        public Boat GetBoatById(int id)
+
+        {
+            if (_boatList.ContainsKey(id))
+            {
+                return _boatList[id];
+            }
+            return null;
+
+
+         // Så man kan finde både ud fra type
+
+            List<Boat> GetBoatByEnum(BoatType boatEnum)
+            {
+                List<Boat> list = new List<Boat>();
+
+                foreach (KeyValuePair<int, Boat> kvp in _boatList)
+
+                {
+                    Boat boat = kvp.Value;
+                    if (boat.Type == boatEnum)
+                        list.Add(boat);
+
+
+                }
+                return list;
+
+
+            }
+
+
+        }
+
+    }
+}
