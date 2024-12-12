43d847f0ea9a2df57e389c1bf2c1c13396a1630f
Nicolei Sander Dalgaard <nid003@edu.zealand.dk>
2024-12-12 12:08:53 +0100

    Blog

--- HillerodConsole/Program.cs
+++ HillerodConsole/Program.cs
@@ -5,6 +5,6 @@
 Class1 class1 = new Class1();
 BoatRepo repo = new BoatRepo();
 
-repo.AddBoat(new(1, "Molly", "SailBoat", "Beneteau395", "395", "Yanmar 4JH4", "12", "2018"));
-repo.AddBoat(new(2, "Dori", "Sailboat", "Shantau245", "245", "Volvo 4kMA", "14", "2014"));
-repo.AddBoat(new(3, "Maren", "Sailboat", "Nimbus 405 Coupe", "N405", "2 x Volvo Penta 380HK", "16", "2020"));
+repo.AddBoat(new(1, "Molly", BoatType.SailBoat, "Beneteau395", "395", "Yanmar 4JH4", 12, "2018"));
+repo.AddBoat(new(2, "Dori", BoatType.SailBoat, "Shantau245", "245", "Volvo 4kMA", 14, "2014"));
+repo.AddBoat(new(3, "Maren", BoatType.MotorBoat, "Nimbus 405 Coupe", "N405", "2 x Volvo Penta 380HK", 16, "2020"));
